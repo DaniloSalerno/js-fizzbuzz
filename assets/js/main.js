d17349fcd5b310f62319c80546de240424c8cded
DaniloSalerno <salerno.danilo@hotmail.it>
2023-08-25 15:06:28 +0200

    set color for correct number

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-//Scrivi un programma che stampi in console i numeri da 1 a 100
-
-//per i multipli di 3 stampi “Fizz” al posto del numero
-
-//per i multipli di 5 stampi “Buzz”.
-
-//Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-
 /*BONUS 1:
 Crea un container nel DOM ,
 
@@ -15,22 +7,36 @@ BONUS 2:
 Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1,
  a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. 
  Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
+const rowElement = document.querySelector('.row');
+
 
  for (let i = 1; i <= 100; i++) {
 
+     
+     let colorStyle;
+     
     if (!(i % 3) && !(i % 5)) {
 
+        colorStyle = 'bg-success';
         console.log('FizzBuzz');
 
     } else if (!(i % 3)) {
-
+        
+        colorStyle = 'bg-warning';
         console.log('Fizz');
 
     } else if (!(i % 5)) {
 
+        colorStyle = 'bg-danger';
         console.log('Buzz');
 
     } else {
+        
+        colorStyle = 'bg-primary';
         console.log(i);
+        
     }
+    rowElement.innerHTML += `<div class="col ${colorStyle}">${i}</div>`;
+
+
  }
