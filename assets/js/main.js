bd57dc8fd267eeb3a637324f419465819a74b8a8
DaniloSalerno <salerno.danilo@hotmail.it>
2023-08-25 15:47:31 +0200

    change content in the box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,36 @@
-/*BONUS 1:
-Crea un container nel DOM ,
-
-aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
-
-BONUS 2:
-Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1,
- a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. 
- Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna. */
 const rowElement = document.querySelector('.row');
 
 for (let i = 1; i <= 100; i++) {
-    //const boxContent = document.querySelector('.square');
+
     let colorStyle;
-     
+    let boxContent;
     if (!(i % 3) && !(i % 5)) {
 
-        //boxContent.textContent = 'FizzBuzz';
         colorStyle = 'bg-success';
         console.log('FizzBuzz');
+        boxContent = 'FizzBuzz';
 
     } else if (!(i % 3)) {
 
-        //boxContent.textContent = 'Fizz';
         colorStyle = 'bg-warning';
         console.log('Fizz');
+        boxContent = 'Fizz';
 
     } else if (!(i % 5)) {
-        //boxContent.textContent = 'Buzz';
+
         colorStyle = 'bg-danger';
         console.log('Buzz');
+        boxContent = 'Buzz';
 
     } else {
         
         colorStyle = 'bg-primary';
         console.log(i);
+        boxContent = i;
         
     }
 
-    rowElement.innerHTML += `<div class="square ${colorStyle}">${i}</div>`;
+    rowElement.innerHTML += `<div class="square ${colorStyle}">${boxContent}</div>`;
 
 
  }
